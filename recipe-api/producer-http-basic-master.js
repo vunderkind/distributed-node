50b753284269bbc07b37f54f04618ee7afa91548
Justin Irabor <justin.irabor@gmail.com>
2022-02-06 21:35:35 +0100

    Done for today

---
+++ recipe-api/producer-http-basic-master.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+const cluster = require('cluster');
+console.log(`master pid=${process.pid}`);
+cluster.setupMaster({
+    exec: __dirname + '/producer-http-basic.js',
+});
+cluster.fork();
+cluster.fork();
+cluster
+    .on('disconnect', worker => {
+        console.log(`disconnect, ${worker.id}`);
+    })
+    .on('exit', (worker, code, signal) => { 
+        console.log(`exit, ${worker.id}, code=${code}, signal=${signal}`);
+        cluster.fork();
+    })
+    .on('listening', (worker, { address, port }) => {
+        console.log(`listening, ${worker.id}, address=${address}, port=${port}`);
+    });
